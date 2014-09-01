0d4ad6e24fdf9d08748624bec7f386b5716566a5
gian michele innocenti <ginnocen@cern.ch>
2014-09-01 15:44:18 +0200

    add

--- TSrepo/CelloRSCMP7Compare.cc
+++ TSrepo/CelloRSCMP7Compare.cc
@@ -19,7 +19,6 @@ rcttsWorkerCell::CelloRSCMP7Compare::CelloRSCMP7Compare(Logger& log, tsframework
   logger_ = Logger::getInstance(log.getName() +".CelloRSCMP7Compare");
   
   for (int i=0;i<10;i++){
-      
     stringstream ss;
     ss << "Link" <<i;
     getParamList().insert(std::make_pair(ss.str(),new xdata::String("")));
@@ -40,22 +39,27 @@ void rcttsWorkerCell::CelloRSCMP7Compare::code()
 { 
   
   std::vector < std::vector < unsigned int > > myvectorLink;
-  int value;
 
   for (int i=0;i<10;i++){
     stringstream sst;
+    stringstream test;
     sst << "Link" <<i;
-    string mystringLink = getParamList()[sst.str()]->toString();
-    istringstream issLink(mystringLink);
+    string mystringLink = dynamic_cast<xdata::String*>(getParamList()[sst.str()])->toString();
+    test<<mystringLink;
+    if (!mystringLink.empty()) payload_ = new xdata::String(test.str());   
     
-    while(issLink){
-     issLink >> value;
-     myvectorLink[i].push_back(value);
-    }  
-  }
+    //std::istringstream iss (mystringLink);
+    //for (int n=0; n<10; n++){int val; iss >> val; myvectorLink[i].push_back(val); }
+ }
+//    for (int n=0; n<1024; n++){
+//      int val;
+//      iss >> val;
+//    }
+//  }
+
     //LOG4CPLUS_INFO((logger_),  "test:"  << endl);
 
-    if(1) payload_ = new xdata::String("ALL larger version  FFssFFF");
-    else payload_ = new xdata::String("b<0 TRIS!");
+    //if(1) payload_ = new xdata::String("ALL larger version  affanculo2");
+    //else payload_ = new xdata::String("b<0 TRIS!");
   
  }
