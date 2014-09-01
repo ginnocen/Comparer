8460ea5b3f50bba9476d5b1279d8d5160dde26cf
gian michele innocenti <ginnocen@cern.ch>
2014-09-01 16:33:13 +0200

    add

--- TSrepo/CelloRSCMP7Compare.cc
+++ TSrepo/CelloRSCMP7Compare.cc
@@ -6,6 +6,7 @@
 #include "xcept/tools.h"
 #include <boost/lexical_cast.hpp>
 #include <string>
+#include <vector>
 #include <sstream>
 #include "toolbox/string.h"
 #include "log4cplus/logger.h"
@@ -37,31 +38,35 @@ rcttsWorkerCell::CelloRSCMP7Compare::~CelloRSCMP7Compare()
 
 void rcttsWorkerCell::CelloRSCMP7Compare::code()
 { 
-  
+  const int NintsPerLink=1024;
+  unsigned int array[NintsPerLink];
   std::vector < std::vector < unsigned int > > myvectorLink;
+  std::vector<unsigned int> tempVector;
+  tempVector.resize(sizeof(array));
+  myvectorLink.resize(6*sizeof(tempVector));
+  for(unsigned int i = 0; i<10; i++) myvectorLink[i].reserve(sizeof(array));
 
-  for (int i=0;i<1;i++){
+  unsigned int val; 
+  for (int i=0;i<10;i++){
     stringstream sst;
     stringstream test;
     sst << "Link" <<i;
     string mystringLink = dynamic_cast<xdata::String*>(getParamList()[sst.str()])->toString();
     test<<mystringLink;
-    if (!mystringLink.empty()) payload_ = new xdata::String(test.str());   
+    //if (!mystringLink.empty()) payload_ = new xdata::String(test.str());   
     
     std::istringstream iss (mystringLink);
-    //for (int n=0; n<10; n++){int val; iss >> val; myvectorLink[i].push_back(val); }
-    for (int n=0; n<10; n++){
-      int val; 
+    for (int n=0; n<NintsPerLink; n++){
       iss >> val; 
+      myvectorLink[i].push_back(val); 
     }
  }
-//    for (int n=0; n<1024; n++){
-//      int val;
-//      iss >> val;
-//    }
-//  }
-
-    //LOG4CPLUS_INFO((logger_),  "test:"  << endl);
+ 
+   stringstream last;
+   last << "Value addss" <<myvectorLink[3][888];
+   payload_ = new xdata::String(last.str());   
+   
+   //LOG4CPLUS_INFO((logger_),  "test:"  << endl);
 
     //if(1) payload_ = new xdata::String("ALL larger version  affanculo2");
     //else payload_ = new xdata::String("b<0 TRIS!");
